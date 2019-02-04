111181fbe019ab84d61a80e12fafe0ac09515ce5
samtkaplan <sam.kaplan@chevron.com>
2019-02-04 07:18:21 -0600

    using internal methods messes up type inference in DistributedJets

--- src/jop_blend.jl
+++ src/jop_blend.jl
@@ -17,32 +17,6 @@ d = A*m # receiver gather with blended shots
 ```
 """
 function JopBlend(T, nsamples, shottimes, nsamples_blended=0)
-    function df!(d::AbstractArray{T,1}, m::AbstractArray{T,2}; st, kwargs...) where {T}
-        d .= 0
-        nsamples, nshots = size(m)
-        nsamples_blended = length(d)
-        for ishot = 1:nshots, isample = 1:nsamples
-            j = st[ishot] + isample - 1
-            if j ∈ 1:nsamples_blended
-                d[j] += m[isample,ishot]
-            end
-        end
-        d
-    end
-
-    function df′!(m::AbstractArray{T,2}, d::AbstractArray{T,1}; st, kwargs...) where {T}
-        m .= 0
-        nsamples, nshots = size(m)
-        nsamples_blended = length(d)
-        for ishot = 1:nshots, isample = 1:nsamples
-            j = st[ishot] + isample - 1
-            if j ∈ 1:nsamples_blended
-                m[isample,ishot] = d[j]
-            end
-        end
-        m
-    end
-
     if nsamples_blended == 0
         nsamples_blended = maximum(shottimes) + nsamples - 1
     end
@@ -50,6 +24,32 @@ function JopBlend(T, nsamples, shottimes, nsamples_blended=0)
     dom = JetSpace(T, nsamples, length(shottimes))
     rng = JetSpace(T, nsamples_blended)
 
-    JopLn(df! = df!, df′! = df′!, dom = dom, rng = rng, s = (st=shottimes,))
+    JopLn(df! = JopBlend_df!, df′! = JopBlend_df′!, dom = dom, rng = rng, s = (st=shottimes,))
 end
 export JopBlend
+
+function JopBlend_df!(d::AbstractArray{T,1}, m::AbstractArray{T,2}; st, kwargs...) where {T}
+    d .= 0
+    nsamples, nshots = size(m)
+    nsamples_blended = length(d)
+    for ishot = 1:nshots, isample = 1:nsamples
+        j = st[ishot] + isample - 1
+        if j ∈ 1:nsamples_blended
+            d[j] += m[isample,ishot]
+        end
+    end
+    d
+end
+
+function JopBlend_df′!(m::AbstractArray{T,2}, d::AbstractArray{T,1}; st, kwargs...) where {T}
+    m .= 0
+    nsamples, nshots = size(m)
+    nsamples_blended = length(d)
+    for ishot = 1:nshots, isample = 1:nsamples
+        j = st[ishot] + isample - 1
+        if j ∈ 1:nsamples_blended
+            m[isample,ishot] = d[j]
+        end
+    end
+    m
+end
--- src/jop_circshift.jl
+++ src/jop_circshift.jl
@@ -1,7 +1,7 @@
 function JopCircShift(spc, shifts)
-    df!(d, m; shifts_forward, kwargs...) = circshift!(d, m, shifts_forward)
-    df′!(m, d; shifts_adjoint, kwargs...) = circshift!(m, d, shifts_adjoint)
-
-    JopLn(df! = df!, df′! = df′!, dom = spc, rng = spc, s = (shifts_forward=shifts,shifts_adjoint=map(-, shifts)))
+    JopLn(df! = JopCircShift_df!, df′! = JopCircShift_df′!, dom = spc, rng = spc, s = (shifts_forward=shifts,shifts_adjoint=map(-, shifts)))
 end
 export JopCircShift
+
+JopCircShift_df!(d, m; shifts_forward, kwargs...) = circshift!(d, m, shifts_forward)
+JopCircShift_df′!(m, d; shifts_adjoint, kwargs...) = circshift!(m, d, shifts_adjoint)
