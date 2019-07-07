da6c3d87240cdf6548057f753522ec13034bb5a2
wask <wask@chevron.com>
2019-07-07 16:30:47 -0500

    initial commit for power operator f(x) = (x/c)^a

---
+++ src/jop_pow.jl
@@ -0,0 +1,11 @@
+"""
+    F = JopPow(spc, c)
+
+where `F` is the power operator (c/x)^a with domain and range given by `spc::JetSpace`, and scalar values c, a.
+"""
+JopPow(spc::JetSpace{T}, c = 1, a = 2) where {T} = JopNl(dom = spc, rng = spc, f! = JopPow_f!, df! = JopPow_df!, df′! = JopPow_df′!, s = (c=T(c), a=T(a)))
+export JopPow
+
+JopPow_f!(d::AbstractArray, m::AbstractArray; c, a) = d .= (m ./ c).^a
+JopPow_df!(δd::AbstractArray, δm::AbstractArray; mₒ, c, a) = δd .= ((a/c) .* (mₒ ./ c).^(a-1)) .* δm
+JopPow_df′!(δm::AbstractArray, δd::AbstractArray; mₒ, c, a)= δm .= conj((a/c) .* (mₒ ./ c).^(a-1)) .* δd
