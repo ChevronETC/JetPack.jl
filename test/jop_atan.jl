fb32c8db64a2f43ba276773a72275d6eec3e1b24
wask <wask@chevron.com>
2019-07-08 12:16:34 -0500

    scale input reference model for linearization test to assure successful completion

--- test/jop_atan.jl
+++ test/jop_atan.jl
@@ -1,4 +1,4 @@
-using LinearAlgebra, Jets, JetPack, Test
+using LinearAlgebra, Jets, JetPack, Test, Printf
 
 n1,n2 = 33,44
 
@@ -24,7 +24,7 @@ end
 
 @testset "JopAtan, linearization test, T=$(T)" for T in (Float64,Float32,Complex{Float64},Complex{Float32})
     F = JopAtan(JetSpace(T,n1,n2))
-    m0 = rand(domain(F))
+    m0 = 0.5 .* (-1 .+ rand(domain(F)))
     μ  = sqrt.([1/1,1/2,1/4,1/8,1/16,1/32,1/64,1/128,1/256,1/512,1/1024,1/2048,1/4096,1/8192])
     δm = rand(domain(F))
     observed, expected = linearization_test(F, m0, μ = μ, δm = δm)
