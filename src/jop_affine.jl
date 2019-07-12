3e5f0a890828442ac5186e7f3eb8164f4cf30cf5
Anusha.Sekar@chevron.com <Anusha.Sekar@chevron.com>
2019-07-12 15:25:58 -0500

    Using self adjoint nature and removing df!

--- src/jop_affine.jl
+++ src/jop_affine.jl
@@ -27,14 +27,9 @@ m = ones(domain(A))
 d = A*m # d = [2.0 ; 3.0 ; 4.0]
 ```
 """
-function JopAffine(shift::AbstractArray{T}) where {T}
-    spc = JetSpace(T, size(shift))
-    JopNl(f! = JopAffine_f!, df! = JopAffine_df!, df′! = JopAffine_df′!, dom = spc, rng = spc, s = (shift=shift,))
-end
-
-JopAffine(spc::JetAbstractSpace, shift) = JopNl(f! = JopAffine_f!, df! = JopAffine_df!, df′! = JopAffine_df′!, dom = spc, rng = spc, s = (shift=shift,))
+JopAffine(spc::JetAbstractSpace, shift) = JopNl(f! = JopAffine_f!, df! = JopAffine_df!, dom = spc, rng = spc, s = (shift=shift,))
+JopAffine(shift::AbstractArray{T}) where {T} = JopAffine( JetSpace(T, size(shift)),shift)
 export JopAffine
 
 JopAffine_f!(d, m; shift, kwargs...) = d .=  m .+ shift
 JopAffine_df!(δd, δm; mₒ, shift, kwargs...) = δd .=  δm #Derivative is identity
-JopAffine_df′!(δm, δd; mₒ, shift, kwargs...) = δm .=  δd #Adjoint of derivative is identity
