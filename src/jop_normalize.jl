e2201a649ff95bd25832fef32c1af0dfb3e8d32c
wask <wask@chevron.com>
2019-07-21 13:30:26 -0500

    passing correctness, linearity, dot product, and linearization tests!

--- src/jop_normalize.jl
+++ src/jop_normalize.jl
@@ -1,5 +1,3 @@
-    using Printf
-
 """
     F = JopNormalize(spc, c)
 
@@ -14,67 +12,28 @@ function JopNormalize(spc::JetSpace{T}; α=1.0) where {T}
 end
 export JopNormalize
 
-# d(t) = m(t) / [int_0^T m(t)]
 function JopNormalize_f!(d::AbstractArray, m::AbstractArray; tmp1, tmp2, A)
-    d .= 0
-    n1 = size(d,1)
-    n2 = div(length(d),n1)
-    _d = reshape(d,n1,n2)
-    _m = reshape(m,n1,n2)
-    tmp1 .= A * m
-    for k2 = 1:n2
-        d[:,k2] .= _m[:,k2] ./ tmp1[end,k2]
-    end
-    d 
+    d .= m ./ sum(m, dims=1)
 end
 
-# d(t)  = m(t) / [int_0^T m(t)]
-# d'(t) = m(t) (1 / [int_0^T m(t)])' + 1 / [int_0^T m(t)]
+# For normalized integral method:
+#   f   = I[m] / I_t[m]
+#   df  = [ I[m]' I_t[m] - I[m] I_t[m]' ] / I_t[m]^2
+#   df  = (I_t[m] I[dm] - I_t[dm] I[m]) / (I_t[m])^2
+#   dfᵀ = (I_t[m] Iᵀ[δd] - I_tᵀ[mᵀ Iᵀ δd]) / (I_t[m])^2
+#
+# For normalization only:
+#   f   = m / I_t[m]
+#   df  = [ m' I_t[m] - m I_t[m]' ] / I_t[m]^2
+#   df  = ( I_t[m] dm - I_t[dm] m) / (I_t[m])^2
+#   dfᵀ = (I_t[m] δd - I_tᵀ[mᵀ δd]) / (I_t[m])^2
 
-# (-I_t[dm] I[m] + I_t[m] I[dm]) / (I_t[m])^2
-#δd .= (-sum(δm,dims=1) .* cumsum(mₒ,dims=1) .+ sum(mₒ,dims=1) .* cumsum(δm,dims=1)) ./ sum(mₒ,dims=1).^2
+# (I_t[m] I[dm] - I_t[dm] I[m]) / (I_t[m])^2
 function JopNormalize_df!(δd::AbstractArray{T}, δm::AbstractArray{T}; mₒ, tmp1, tmp2, A) where {T}
-    n1 = size(δd,1)
-    n2 = div(length(δd),n1)
-    _δd = reshape(δd,n1,n2)
-    _δm = reshape(δm,n1,n2)
-    tmp1 .= A * mₒ
-    tmp2 .= A * δm
-    for k2 = 1:n2
-        _δd[:,k2] .= (-tmp2[end,k2] .* tmp1[:,k2] .+ tmp1[end,k2] .* tmp2[:,k2]) ./ tmp1[end,k2]^2
-    end
-    δd 
+    δd .= (sum(mₒ,dims=1) .* δm .- sum(δm,dims=1) .* mₒ) ./ sum(mₒ,dims=1).^2
 end
 
-# (-I_tᵀ[mᵀIᵀ] + I_t[m] Iᵀ) δd / (I_t[m])^2
-#δm .= conj.(( -sum( mₒ .* (A'*conj.(δd)) ,dims=1) .+ ( sum(mₒ,dims=1) .* (A'*conj.(δd)) ) ) ./ sum(mₒ,dims=1).^2)
+# (I_t[m] Iᵀ[δd] - I_tᵀ[mᵀ Iᵀ δd]) / (I_t[m])^2
 function JopNormalize_df′!(δm::AbstractArray{T}, δd::AbstractArray{T}; mₒ, tmp1, tmp2, A) where {T}
-    n1 = size(δd,1)
-    n2 = div(length(δd),n1)
-    _δd = reshape(δd,n1,n2)
-    _δm = reshape(δm,n1,n2)
-
-    d1 = zeros(domain(A))
-    d2 = zeros(domain(A))
-
-    tmp_Atcd = A' * conj.(δd)
-    tmp1 .= A * mₒ
-    tmp2 .= A * (mₒ .* tmp_Atcd)
-    for k2 = 1:n2
-        _δm[:,k2] .= conj.(( -1 .* tmp2[end,k2] .+ (tmp1[end,k2] .* tmp_Atcd[:,k2]) ) ./ tmp1[end,k2].^2)
-
-        d1[:,k2] .= tmp2[end,k2]
-    end
-
-    d2 .= sum( mₒ .* (A'*conj.(δd)) ,dims=1)
-
-    for k2 = 1:n2
-        @info @sprintf(" ")
-        @info @sprintf("k2,n2,d1; %3d %3d %+12.6f %+12.6f %+12.6f %+12.6f %+12.6f", k2, n2, d1[:,k2]...)
-        @info @sprintf("k2,n2,d2; %3d %3d %+12.6f %+12.6f %+12.6f %+12.6f %+12.6f", k2, n2, d2[:,k2]...)
-    end
-
-    #δm .= conj.(( -sum( mₒ .* (A'*conj.(δd)) ,dims=1) .+ ( sum(mₒ,dims=1) .* (A'*conj.(δd)) ) ) ./ sum(mₒ,dims=1).^2)
-
-    δm 
+    δm .= conj.((sum(mₒ,dims=1) .* conj.(δd) .- sum(mₒ .* conj.(δd),dims=1)) ./ sum(mₒ,dims=1).^2)
 end
--- test/jop_normalize.jl
+++ test/jop_normalize.jl
@@ -9,7 +9,7 @@ n1,n2 = 5,2
     @test F*x1 ≈ ones(domain(F)) ./ n1
 end
 
-@test_skip @testset "JopNormalize, linearity test, T=$(T)" for T in (Float32, Float64, Complex{Float32}, Complex{Float64})
+@testset "JopNormalize, linearity test, T=$(T)" for T in (Float32, Float64, Complex{Float32}, Complex{Float64})
     F = JopNormalize(JetSpace(T,n1,n2))
     J = jacobian(F, rand(domain(F)))
     lhs,rhs = linearity_test(J)
@@ -18,7 +18,7 @@ end
     @test lhs ≈ rhs
 end
 
-@test_skip @testset "JopNormalize, dot product test, T=$(T)" for T in (Float32, Float64, Complex{Float32}, Complex{Float64})
+@testset "JopNormalize, dot product test, T=$(T)" for T in (Float32, Float64, Complex{Float32}, Complex{Float64})
     F = JopNormalize(JetSpace(T,n1,n2))
     J  = jacobian!(F, rand(domain(F)))
     lhs, rhs = dot_product_test(J, -1 .+ 2 .* rand(domain(J)), -1 .+ 2 .* rand(range(J)))
@@ -26,7 +26,7 @@ end
 end
 
 # note the key here is to increase the size of the nonlinear vector
-@test_skip @testset "JopNormalize, linearization test, T=$(T)" for T in (Float32, Float64, Complex{Float32}, Complex{Float64})
+@testset "JopNormalize, linearization test, T=$(T)" for T in (Float32, Float64, Complex{Float32}, Complex{Float64})
     F = JopNormalize(JetSpace(T,n1,n2))
     m0 = rand(domain(F))
     μ  = sqrt.([1/1,1/2,1/4,1/8,1/16,1/32,1/64,1/128,1/256,1/512,1/1024,1/2048,1/4096,1/8192])
