f635f50cdac5d76d1f2ae07a63a9af39956e5c08
samkaplan <sam.kaplan@chevron.com>
2022-08-10 11:43:18 +0000

    add docstring for JopReshape

--- src/jop_reshape.jl
+++ src/jop_reshape.jl
@@ -1,3 +1,18 @@
+"""
+    A = JopReshape(dom, rng)
+
+Reshape an array that belongs in `dom::JetAbstractSpace` to one that belongs to `rng::JetAbstractSpace`
+
+# Example
+```julia
+using Jets, JetPack
+dom = JetSpace(Float32,10,20)
+rng = JetSpace(Float32,200)
+A = JopReshape(JetSpace(Float32,10,20), JetSpace(Float32,200))
+x = rand(domain(A))
+y = A*x
+```
+"""
 JopReshape(dom::JetAbstractSpace, rng::JetAbstractSpace) = JopLn(dom = dom, rng = rng, df! = JopReshape_df!)
 
 export JopReshape
